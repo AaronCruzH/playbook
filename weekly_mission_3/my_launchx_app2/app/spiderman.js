889c796f3929073601039957ae6b0acb25709e67
[AaronCruzH] <[fraronzez@gmail.com]>
2022-05-19 10:27:16 -0500

    Prueba2 classSpiderman

--- weekly_mission_3/my_launchx_app2/app/spiderman.js
+++ weekly_mission_3/my_launchx_app2/app/spiderman.js
@@ -1,5 +1,11 @@
 class spiderman{
-    constructor(name){this.name=name}
+    constructor(name,age,actor,movies,studio){
+        this.name=name
+        this.age=age
+        this.actor=actor
+        this.movies=movies
+        this.studio=studio
+    }
 }
 
 module.exports = spiderman
