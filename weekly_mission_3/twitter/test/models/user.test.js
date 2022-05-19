9b0d02ff6f4b66c5e7d8e02668bd3ff5a57d0b30
[AaronCruzH] <[fraronzez@gmail.com]>
2022-05-19 13:40:07 -0500

    modificaion1 user.test.js

--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -3,13 +3,12 @@ const User = require('./../../app/models/user')
 describe("Unit Tests for User class", () => {
 
   test('1) Create an User object', () => {
-    const user = new User(777,"CruzAaronFco","Aaron","Bio","dateCreated","lastUpdated")
-
+    const user = new User(777,"CruzAaronFco","Aaron","Bio")
     expect(user.id).toBe(777)
     expect(user.username).toBe("CruzAaronFco")
     expect(user.name).toBe("Aaron")
     expect(user.bio).toBe("Bio")
-    expect(user.dateCreated).toBe("dateCreated")
-    expect(user.lastUpdated).toBe("lastUpdated")
+    expect(user.dateCreated).not.toBe("dateCreated")
+    expect(user.lastUpdated).not.toBeUndefined("lastUpdated")
   });
 })  
