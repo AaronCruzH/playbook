82c1ec3086e7f17770ab9c9be7f5cc530d973225
[AaronCruzH] <[fraronzez@gmail.com]>
2022-05-19 15:11:07 -0500

    app/view/UserView1.1

--- weekly_mission_3/twitter/app/view/UserView.js
+++ weekly_mission_3/twitter/app/view/UserView.js
@@ -6,7 +6,9 @@ class UserView{
     if(payload === null){
       console.log("Error es null")
       return {error: "El payload no existe."}
-    }
+    }else{
+        return {error: "Error, las propiedades del payload necesitan tener un valor válido"}
+      }
 }
 }
 
